d6bbc718d491a51f3603c8b479400840cb53805c
Bob Frankston <Github@bob.ma>
2026-03-16 13:19:18 -0400

    Pre-release commit

--- test.bat
+++ test.bat
@@ -1,16 +1,17 @@
 cls
 set dest=172.20.4.58
+set trace=
 
 call C:\Users\Bob\AppData\Roaming\npm\npmglobalize.cmd
 ping %dest% -n 1
-neoping.ps1 %dest% -trace -c 1
-neoping -v
+call C:\Users\Bob\AppData\Roaming\npm\neoping.ps1 %dest% %trace% -c 1
+call C:\Users\Bob\AppData\Roaming\npm\neoping -v
 
 wsl ping %dest% -c 1
-wsl neoping %dest% -trace -c 1
+wsl neoping %dest% %trace% -c 1
 wsl neoping -v
 
 ssh pi4c sudo npm install -g @bobfrankston/neoping
 ssh pi4c ping %dest% -c 1
-ssh pi4c neoping %dest% -trace -c 1
+ssh pi4c neoping %dest% %trace% -c 1
 ssh pi4c neoping -v
